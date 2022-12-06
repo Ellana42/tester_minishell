152061095c10e5c203015fa18e54d2f2b1b59393
Ellana42 <mtmrkaploun@gmail.com>
2022-12-06 14:24:13 +0100

    Fixes

--- test.py
+++ test.py
@@ -1,21 +1,10 @@
-# TODO print expected output
-# TODO launch with valgrind
-# TODO check leaks output
-# TODO print leaks output
-import math
 import os
 
 import pandas as pd
 from termcolor import colored
 
-tests = pd.read_csv("tests.csv", index_col=0)
-test_material = tests[["entry", "output"]]
-print(test_material)
 
-path_exec = "./minishell/srcs/parsing/a.out"
-
-
-def test_valgrind(test):
+def test_valgrind(entry):
     command = f'colour-valgrind {path_exec} "{entry}"'
     stream = os.popen(command)
     output = stream.read()
@@ -25,21 +14,49 @@ def test_valgrind(test):
         print(colored("HAS LEAKS", "red"))
 
 
-expected_output = ""
-start = 86
-for i, test in test_material.iterrows():
-    if i >= start:
-        print(50 * "-" + f" {i}")
-        entry = test["entry"][3:].replace("$", "\\$").replace('"', '\\"')
-        if type(test["output"]) == str:
-            expected_output = test["output"]
-        command = f'{path_exec} "{entry}"'
-        # print(f"The command : {command}")
-        stream = os.popen(command)
-        output = stream.read()
-        print(colored(20 * ">", "magenta"))
-        print(colored(output, "magenta"))
-        print(colored(20 * ">", "magenta"))
-        print(colored(expected_output, "blue"))
-        test_valgrind(test)
-        input()
+def full_test(test_material, start=0):
+    expected_output = ""
+    for i, test in test_material.iterrows():
+        if i >= start:
+            print(50 * "-" + f" {i}")
+            entry = test["entry"][3:].replace("$", "\\$").replace('"', '\\"')
+            print(colored(entry, "blue"))
+            if type(test["output"]) == str:
+                expected_output = test["output"]
+            command = f'{path_exec} "{entry}"'
+            # print(f"The command : {command}")
+            stream = os.popen(command)
+            output = stream.read()
+            print(colored(20 * ">", "magenta"))
+            print(colored(output, "magenta"))
+            print(colored(20 * ">", "magenta"))
+            print(colored(expected_output, "blue"))
+            test_valgrind(entry)
+            input()
+
+
+def list_issues(test_material, start=0):
+    pb_index = []
+    for i, test in test_material.iterrows():
+        if i >= start:
+            entry = test["entry"][3:].replace("$", "\\$").replace('"', '\\"')
+            if type(test["output"]) == str:
+                expected_output = test["output"]
+                command = f'colour-valgrind {path_exec} "{entry}"'
+                stream = os.popen(command)
+                output = stream.read()
+                if not "no leaks are possible" in output:
+                    pb_index.append(i)
+    return pb_index
+
+
+if __name__ == "__main__":
+    tests = pd.read_csv("tests.csv", index_col=0)
+    test_material = tests[["entry", "output"]]
+    # print(test_material)
+
+    path_exec = "./minishell/srcs/a.out"
+    full_test(test_material, start=0)
+    # issues = list_issues(test_material)
+    # print(issues)
+    # print(len(issues))
